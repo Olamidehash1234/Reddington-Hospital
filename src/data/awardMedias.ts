524590d83f10b993ee1851359811a54f4786614c
olamidehash1234 <olamideadeola2005@gmail.com>
2026-04-02 00:44:48 +0100

    Done with the awards media section on the awards page

---
+++ src/data/awardMedias.ts
@@ -0,0 +1,102 @@
+export const awardMedias = [
+    {
+        id: 1,
+        image: '/image/Awards/awd-1.png',
+        title: 'Hospital Event 2024'
+    },
+    {
+        id: 2,
+        image: '/image/Awards/awd-2.png',
+        title: 'Medical Excellence Team'
+    },
+    {
+        id: 3,
+        image: '/image/Awards/awd-3.png',
+        title: 'Award Ceremony 2023'
+    },
+    {
+        id: 4,
+        image: '/image/Awards/awd-4.png',
+        title: 'Healthcare Leadership'
+    },
+    {
+        id: 5,
+        image: '/image/Awards/awd-5.png',
+        title: 'Facility Recognition'
+    },
+    {
+        id: 6,
+        image: '/image/Awards/awd-6.png',
+        title: 'Team Celebration'
+    },
+    {
+        id: 7,
+        image: '/image/Awards/awd-7.png',
+        title: 'Professional Recognition'
+    },
+    {
+        id: 8,
+        image: '/image/Awards/awd-8.png',
+        title: 'Hospital Achievement'
+    },
+    {
+        id: 9,
+        image: '/image/Awards/awd-9.png',
+        title: 'Innovation in Healthcare'
+    },
+    {
+        id: 10,
+        image: '/image/Awards/awd-10.png',
+        title: 'Hospital Achievement'
+    }, {
+        id: 11,
+        image: '/image/Awards/awd-11.png',
+        title: 'Hospital Achievement'
+    }, {
+        id: 12,
+        image: '/image/Awards/awd-12.png',
+        title: 'Hospital Achievement'
+    }, {
+        id: 13,
+        image: '/image/Awards/awd-13.png',
+        title: 'Hospital Achievement'
+    }, {
+        id: 14,
+        image: '/image/Awards/awd-14.png',
+        title: 'Hospital Achievement'
+    },
+    {
+        id: 15,
+        image: '/image/Awards/awd-15.png',
+        title: 'Hospital Achievement'
+    },
+    {
+        id: 16,
+        image: '/image/Awards/awd-16.png',
+        title: 'Hospital Achievement'
+    },
+    {
+        id: 17,
+        image: '/image/Awards/awd-17.png',
+        title: 'Hospital Achievement'
+    },
+    {
+        id: 18,
+        image: '/image/Awards/awd-18.png',
+        title: 'Hospital Achievement'
+    },{
+        id: 19,
+        image: '/image/Awards/awd-19.png',
+        title: 'Hospital Achievement'
+    },
+    {
+        id: 20,
+        image: '/image/Awards/awd-20.png',
+        title: 'Hospital Achievement'
+    },
+    {
+        id: 21,
+        image: '/image/Awards/awd-21.png',
+        title: 'Hospital Achievement'
+    },
+];
